fbb47f8f057895db82e6abfee0410d718f09ecdb
edjanga <emmanuel.djanga@live.be>
2022-06-06 10:59:54 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo offers an interface to practise market making skills.</br>
+Markets are made on a number randomly chosen.</br>
+This is also a way for me to practise my C++ skills.
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+int main(){
+    
+    cout << "======================================================================" << endl;
+    cout << "Welcome to the market making interface." << endl;
+    cout << "======================================================================" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    int fixed_seed;
+    int upperbound;
+    cout << "Please enter a seed: " << endl;
+    cin >> fixed_seed;
+    cout << "Please enter an upper bound: " << endl;
+    cin >> upperbound;
+    cout << "You have chosen " << fixed_seed << " as seed and " << upperbound << " as upper bound." << endl;
+    return 0;
+}
